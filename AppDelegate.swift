10b7b52545e1f2efcaec9ad108feec87702ad553
Marlvin Goremusandu <122947486+Marlvin12@users.noreply.github.com>
2023-05-09 22:36:19 -0500

    Add files via upload

---
+++ AppDelegate.swift
@@ -0,0 +1,31 @@
+//  ViewController.swift
+//  video_player
+
+import AVKit
+import AVFoundation
+
+class ViewController: UIViewController  {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+ 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        playVideo()
+    }
+    
+    private func playVideo(){
+        guard let path=Bundle.main.path(forResource: "test", ofType:"mp4") else {
+            debugPrint("test.mp4 not found")
+            return
+        }
+        let player = AVPlayer(url: URL(fileURLWithPath: path))
+        let playerController = AVPlayerViewController()
+        playerController.player=player
+        present(playerController, animated: true) {
+            player.play()
+        }
+    }
+}
---
+++ ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  videoplayer
+//
+//  Created by Marlvin Goremusandu on 5/5/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
